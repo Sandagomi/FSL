7e570bd61441b28068b1b1fd5755a1279af3878f
sandagomi <sandagomi.v.i@gmail.com>
2025-12-05 19:40:48 +0530

    closure lesson

--- closures/closure.js
+++ closures/closure.js
@@ -1,17 +1,49 @@
 
-function outerFunction(outerValue) {
-    return function innerFunction(innerValue) {
+// function outerFunction(outerValue) {
+//     return function innerFunction(innerValue) {
+//         console.log("hi im from outer function " + outerValue)
+//         console.log("hi im from inner function " + innerValue)
+//     }
+// }
 
-        console.log("hi im from outer function " + outerValue)
-        console.log("hi im from inner function " + innerValue)
-
-    }
-}
-
-const newFunction = outerFunction('sandagomi')
-newFunction("vihanga")
+// const newFunction = outerFunction('sandagomi')
+// newFunction("vihanga")
 
 
 
 //closures - its when the inner function access the outer function variables
 
+// const newOne = (sampleOne) => {
+//     const newTwo = (sampleTwo) => {
+//         console.log("hi im from newOne " + sampleOne)
+//         console.log("hi im from newTwo " + sampleTwo)
+//     }
+//     return newTwo
+// }
+
+// const newThree = newOne("sandagomi")
+// newThree("vihanga")
+
+
+
+
+const createCounter = () => {
+    let count = 0
+
+    return {
+        increment() {
+            count++
+            console.log(count)
+        },
+        decrement() {
+            count--
+            console.log(count)
+        }
+    };
+}
+
+const newCounter = createCounter()
+
+newCounter.increment()
+newCounter.increment()
+newCounter.decrement()
