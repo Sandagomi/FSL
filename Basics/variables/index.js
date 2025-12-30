50d9deb621a413c3097aafc22852776a96a4df06
sandagomi <sandagomi.v.i@gmail.com>
2025-12-30 21:30:17 +0530

    custom functions

--- Basics/variables/index.js
+++ Basics/variables/index.js
@@ -7,8 +7,13 @@ const number = 10
 console.log(customer)
 console.log(number)
 
-//variables
-//
 
 
-//
+
+//custom functions
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+console.log(add(5, 10))
