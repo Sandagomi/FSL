96ef319ac055a3ce63acfe4bf8ae47a4a46d0383
sandagomi <sandagomi.v.i@gmail.com>
2025-12-30 21:56:51 +0530

    functions

---
+++ Basics/Functions/index.js
@@ -0,0 +1,23 @@
+
+//custom functions
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function stringify(num) {
+    return "Result:" + num
+}
+
+//Arrow functions
+
+const addNew = (num1, num2) => {
+    return num1 + num2
+}
+
+
+console.log(add(5, 10))
