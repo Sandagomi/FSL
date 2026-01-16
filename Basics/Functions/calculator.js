5dd9af9595569b1c3cd349b8451e9f4ca059faf2
sandagomi <sandagomi.v.i@gmail.com>
2026-01-16 18:38:17 +0530

    calculator

---
+++ Basics/Functions/calculator.js
@@ -0,0 +1,23 @@
+let defaultstate = 0;
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+
+console.log(add(5, 10))
+console.log(subtract(5, 10))
+console.log(multiply(5, 10))
+console.log(divide(5, 10))
